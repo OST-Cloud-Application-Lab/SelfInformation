645e50537ddb0b6811ccacba2aa5f3f1a761a4b4
Jürg Schleutermann <juerg@narcodex.ch>
2018-04-01 12:16:33 +0200

    Cleaned up Maven build file and restructured source files according to Maven documentation for WAR targets.

--- src/main/java/ch/hsr/dal/CDAR_SelfInformationProperties.java
+++ src/main/java/ch/hsr/dal/CDAR_SelfInformationProperties.java
@@ -4,8 +4,7 @@ import java.util.LinkedHashMap;
 
 /**
  * @author dzigerli@hsr.ch (Daniel Zigerlig)
- *
  */
 public interface CDAR_SelfInformationProperties {
-	public LinkedHashMap<String, String> getProperties();
+    public LinkedHashMap<String, String> getProperties();
 }
